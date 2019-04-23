15b8a1b9f739b3df0b94aa6a0717e46044f70e7a
JeffreyBool <1402992668@qq.com>
2019-04-24 04:49:41 +0800

    基于栈实现有效括号

---
+++ 03-stacks-and-queue/03-a-stack-problem-in-leetcode/solution.go
@@ -0,0 +1,65 @@
+/**
+  * Author: JeffreyBool
+  * Date: 2019/4/24
+  * Time: 04:03
+  * Software: GoLand
+*/
+
+package main
+
+import (
+	"Play-with-Data-Structures/03-stacks-and-queue"
+	"fmt"
+)
+
+//有效括号检查 https://leetcode-cn.com/problems/valid-parentheses/
+func isValid(s string) bool {
+	var arrayStack stack.Stack
+	arrayStack = stack.NewStack()
+	brackets := map[rune]rune{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+	for _,char := range []rune(s){
+		if char == '{' || char == '[' || char == '(' {
+			arrayStack.Push(char)
+		}else{
+			if arrayStack.IsEmpty() {return false}
+			v,_ := arrayStack.Pop()
+			topChar := v.(rune)
+			if  val,ok := brackets[char]; ok && topChar != val{
+				return false
+			}
+		}
+	}
+
+	return arrayStack.IsEmpty()
+}
+
+// go 的字符串实际是 byte 类型组成的切片
+func isVaildRune(s string) bool {
+	var stack []rune
+	brackets := map[rune]rune{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+	for _, char := range []rune(s) {
+		if char == '(' || char == '[' || char == '{' {
+			// 括号左半部直接入栈
+			stack = append(stack, char)
+		}else if len(stack) > 0 && stack[len(stack) -1 ] == brackets[char] {
+			// 括号右半部：栈中有数据，且此元素与栈尾元素相同，出栈
+			stack = stack[:len(stack)-1]
+		}else{
+			return false
+		}
+	}
+
+	return len(stack) == 0
+}
+
+func main()  {
+	fmt.Println(isVaildRune("([])"))
+}
