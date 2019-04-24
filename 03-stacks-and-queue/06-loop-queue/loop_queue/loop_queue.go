e5a7c3edb4286527c6a5b6dfc5c3e850e8480858
JeffreyBool <1402992668@qq.com>
2019-04-24 17:34:41 +0800

    循环队列实现

--- 03-stacks-and-queue/06-loop-queue/loop_queue/loop_queue.go
+++ 03-stacks-and-queue/06-loop-queue/loop_queue/loop_queue.go
@@ -22,6 +22,7 @@ type LoopQueue struct {
 const size  = 10
 
 //初始化循环队列
+// 队列存在以空位，所以参数为20就需要开辟21的位置(因为会浪费一个空间用来做循环队列计算)
 func NewQueue(capacity ...int) (queue queue.Queue) {
 	if len(capacity) > 0 && capacity[0] != 0 {
 		queue = &LoopQueue{
@@ -37,15 +38,23 @@ func NewQueue(capacity ...int) (queue queue.Queue) {
 	return
 }
 
+//获取队列长度
 func (queue *LoopQueue) GetSize() int {
-	panic("implement me")
+	return queue.size
 }
 
+//获取队列容量
+func (queue *LoopQueue) GetCapacity() int {
+	// 与上对应，需要删除空位
+	return cap(queue.data) -1
+}
+
+//判断队列是否为空
 func (queue *LoopQueue) IsEmpty() bool {
-	panic("implement me")
+	return queue.front == queue.tail
 }
 
-func (queue *LoopQueue) Enqueue(v interface{}) error {
+func (queue *LoopQueue) Enqueue(v interface{}) (err error) {
 	panic("implement me")
 }
 
